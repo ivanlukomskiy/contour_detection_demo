faf1fc58ca992e5928f841475b5056fcdba0103e
ivanlukomskiy <ivanlukomskiy@yandex-team.ru>
2023-01-04 17:26:17 +0200

    apply geometry

--- rc/target_udp.py
+++ rc/target_udp.py
@@ -5,7 +5,7 @@ import time
 from constants import MAX_WRIST_ANGLE, MIN_WRIST_ANGLE
 from rc.geometry import sheet_coords_to_angles
 
-SERVER_IP = 'localhost'
+SERVER_IP = '10.81.2.201'
 SERVER_PORT = 5005
 
 
@@ -16,7 +16,7 @@ class RcTarget:
     def apply(self, x, y, z):
         shoulder_angle, elbow_angle = sheet_coords_to_angles(x, y)
         wrist_angle = z * (MAX_WRIST_ANGLE - MIN_WRIST_ANGLE) + MIN_WRIST_ANGLE
-        print(f'sending {shoulder_angle} {elbow_angle} {wrist_angle}')
+        # print(f'sending {shoulder_angle} {elbow_angle} {wrist_angle}')
         self.sock.sendto(struct.pack('ddd', shoulder_angle, elbow_angle, wrist_angle),
                          (SERVER_IP, SERVER_PORT))
 
